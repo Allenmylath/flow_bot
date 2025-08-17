b3a693143094aaf98925b7660e1cb7385d1337b4
allenmylath <allengeorgemylath@gmail.com>
2025-08-17 15:43:08 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,10 @@
+"""
+Handlers package for managing event handlers.
+"""
+
+from .event_handlers import EventHandlers, setup_event_handlers
+
+__all__ = [
+    "EventHandlers",
+    "setup_event_handlers",
+]
